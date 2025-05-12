e2dca5a2fdace8375bb680d36d6ece9e9cd0034e
seungmin0603 <fullstack.ksmtw2221@nave.com>
2025-05-12 18:05:33 +0900

    우여곡절

--- javaStudy/myclass.java
+++
@@ -1,7 +0,0 @@
-package javaStudy;
-
-public class MyClass{
-	public static void main(string[] a) {
-			system.out.print("어려워요??");
-	}
-}
---
+++ javaStudy/seungmin.java
@@ -0,0 +1,10 @@
+package javaStudy;
+
+public class seungmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("안녕하세요");
+	}
+
+}
